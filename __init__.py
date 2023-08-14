8341c0aaa87937d6b42b1fd9f6a76168ec05d477
Gaurav Manek <gaurav@gauravmanek.com>
2023-08-14 21:18:25 +0800

    Remove main logging.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,3 @@
-import traceback
-from pathlib import Path
+from .hplugin import HierarchicalPCBPlugin
 
-with Path("C:\\Users\\User\\Desktop\\hierarchical.log").open("w") as f:
-    f.write("Hello World\n")
-    try:
-        from .hplugin import HierarchicalPCBPlugin
-
-        HierarchicalPCBPlugin().register()
-        f.write("Registered.\n")
-    except Exception as e:
-        f.write(str(e))
-        f.write(traceback.format_exc())
-    finally:
-        f.write("Goodbye World\n")
+HierarchicalPCBPlugin().register()
